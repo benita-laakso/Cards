4971fc86c8b7d0d5d1afa7318f79b6a2a6af7edc
Laakso Benita <benitalaaksofi@yahoo.com>
2019-10-09 13:41:45 +0300

    Updates

---
+++ precache-manifest.956f151f1147f3129567189638e5b8c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430feb3c1fb7d59031d35df5a5cd6875",
+    "url": "/Cards/index.html"
+  },
+  {
+    "revision": "81f65244bc710565bb61",
+    "url": "/Cards/static/css/main.80de04ba.chunk.css"
+  },
+  {
+    "revision": "fbe5485156ae5d448141",
+    "url": "/Cards/static/js/2.a23af5b4.chunk.js"
+  },
+  {
+    "revision": "81f65244bc710565bb61",
+    "url": "/Cards/static/js/main.42db0313.chunk.js"
+  },
+  {
+    "revision": "b0d8bb43f2cfed475763",
+    "url": "/Cards/static/js/runtime~main.6f34a7fa.js"
+  }
+]);
